be0d7db31d5e9d18e294354509b5fbaeffe1b58b
zlavok322 <zarazazaza2010@gmail.com>
2022-10-06 19:01:24 +0300

    готовое дз

--- homework_1/ViewController.swift
+++ homework_1/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    private var countBooks = 0
+    
+    @IBAction func buttonDidTap(_ sender: UIButton) {
+        countBooks += 1
+        label.text = "Значание счетчика \(countBooks)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
